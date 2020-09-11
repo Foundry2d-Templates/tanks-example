8378803e2804469225ad6a068489e7e096afd829
jsn213 <jean-sebastien.nadeau.3@ulaval.ca>
2020-09-11 10:37:04 -0400

    Update .gitignore, .gitmodules, and 396 more files...

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tanks
+
+An example project.
+
+To clone the project and it's versioned contents do:
+`git clone --recursive https://github.com/Foundry2d-Templates/tanks-example.git`
+
+Just to make sure everything is there do:
+`git submodule update --init --recursive`
+
---
+++ khafile.js
@@ -0,0 +1,20 @@
+let project = new Project('Tanks');
+project.addAssets('Assets/**');
+project.addSources('Sources');
+project.addShaders('Shaders/**');
+project.addParameter("-debug");
+
+// Gameplay code should reside in the gameplay folder of the Sources folder at the root
+// Add these parameters to make sure your Traits aren't removed by dead code elimination 
+// project.addParameter('gameplay');
+// project.addParameter("--macro keep('gameplay')");
+
+// project.addParameter("--times");
+// project.addDefine('macro-times');
+
+project.targetOptions.html5.disableContextMenu = true;
+project.targetOptions.html5.webgl = true;
+await project.addProject('Libraries/foundsdk/khafs');
+await project.addProject('Libraries/foundsdk/editor');
+await project.addProject('Libraries/foundsdk/foundry2d');
+resolve(project);
